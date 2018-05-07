1351555dc36bbae932fcc581ab6318ff1b199603
Juanito98 <juanito.timmy@gmail.com>
2018-05-07 17:39:57 -0500

    Updates

--- AndroidApplication/app/src/main/java/jcsiglerp/androidproject/menu.java
+++ AndroidApplication/app/src/main/java/jcsiglerp/androidproject/menu.java
@@ -1,13 +1,20 @@
 package jcsiglerp.androidproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class menu extends AppCompatActivity {
+public class Menu extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+    }
+
+    protected void comprar(View v) {
+        Intent m = new Intent(getApplicationContext(), Comprar.class);
+        startActivity(m);
     }
 }
