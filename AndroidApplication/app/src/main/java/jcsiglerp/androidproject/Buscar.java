b740b394a0f9ec3e2246dfd593fa6fa4ebdeaea0
Juanito98 <jcsiglerp@gmail.com>
2018-05-08 11:21:20 -0500

    Cambios de Cabito

--- AndroidApplication/app/src/main/java/jcsiglerp/androidproject/Buscar.java
+++ AndroidApplication/app/src/main/java/jcsiglerp/androidproject/Buscar.java
@@ -2,6 +2,10 @@ package jcsiglerp.androidproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Buscar extends AppCompatActivity {
 
@@ -9,5 +13,18 @@ public class Buscar extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_buscar);
+
+        EditText search = findViewById(R.id.edBuscar);
+        search.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
+                boolean handled = false;
+                if (i == EditorInfo.IME_ACTION_SEARCH) {
+                    // TU FUNCIÓN DE BUSCAR
+                    handled = true;
+                }
+                return handled;
+            }
+        });
     }
 }
